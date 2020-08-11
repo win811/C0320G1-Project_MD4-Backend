d29b0270c4c81567288bec4a4ac4b4528e8ba311
win811 <cuongwin811@gmail.com>
2020-08-11 09:22:33 +0700

    first time

---
+++ src/main/java/md4/bid_project/BidProjectApplication.java
@@ -0,0 +1,13 @@
+package md4.bid_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/md4/bid_project/BidProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package md4.bid_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
