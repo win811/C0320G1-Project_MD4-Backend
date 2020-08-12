a6aa66f0301aadb677c0583520da093f6412dfb7
hothaodn <hothaodn@gmail.com>
2020-08-12 10:04:11 +0700

    B-Thao add CartDetail class structures

---
+++ src/main/java/md4/bid_project/services/Impl/CartDetailServiceImpl.java
@@ -0,0 +1,8 @@
+package md4.bid_project.services.Impl;
+
+import md4.bid_project.services.CartDetailService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartDetailServiceImpl implements CartDetailService {
+}
