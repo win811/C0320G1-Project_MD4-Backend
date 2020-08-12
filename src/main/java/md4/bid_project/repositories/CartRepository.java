6662205d2d3c7b866ac34404b1765cc48c767576
quandang1996 <62531954+quandang1996@users.noreply.github.com>
2020-08-12 13:56:31 +0700

    Delete CartRepository.java

--- src/main/java/md4/bid_project/repositories/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package md4.bid_project.repositories;
-
-import md4.bid_project.models.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart,Long> {
-}
