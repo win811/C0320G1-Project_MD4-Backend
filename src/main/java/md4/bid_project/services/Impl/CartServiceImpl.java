fbe2b396db6e48e424701702d1239deec66ab82f
quandang1996 <62531954+quandang1996@users.noreply.github.com>
2020-08-12 13:57:20 +0700

    Delete CartServiceImpl.java

--- src/main/java/md4/bid_project/services/Impl/CartServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package md4.bid_project.services.Impl;
-
-import md4.bid_project.services.CartService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CartServiceImpl implements CartService {
-}
