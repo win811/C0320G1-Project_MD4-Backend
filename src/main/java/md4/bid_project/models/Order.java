f9e5ad1e29d5330bf9d7314b775c5fbb55d970d9
quandang1996 <quandanght@gmail.com>
2020-08-12 10:47:04 +0700

    C-Quan add Order(model,repo,service)

--- src/main/java/md4/bid_project/models/Order.java
+++ src/main/java/md4/bid_project/models/Order.java
@@ -3,7 +3,7 @@ package md4.bid_project.models;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.sql.Date;
+import java.util.Date;
 
 @Entity
 @Data
@@ -19,7 +19,7 @@ public class Order {
     private String code;
 
     @Column(name = "order_status")
-    private String status;
+    private boolean status;
 
     @Column(name = "order_payment_method")
     private String paymentMethod;
@@ -33,17 +33,14 @@ public class Order {
     @Column(name = "order_payment_status")
     private boolean paymentStatus;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+
     @Column(name = "order_buyer_id")
     private User user;
 
-    @ManyToOne
-    @JoinColumn(name ="delivery_id")
+
     @Column(name = "order_delivery_address_id")
     private DeliveryAddresses deliveryAddresses;
 
-    @OneToOne(mappedBy = "order")
     @Column(name = "order_cart_id")
     private Carts carts;
 }
