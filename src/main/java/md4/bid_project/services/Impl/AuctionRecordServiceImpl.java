22f377970ac48cae42c184135e10cb53b4f7b663
nancynguyen14701 <ngannguyenqn2001@gmail.com>
2020-08-12 10:59:10 +0700

    C-Ngan add AuctionRecordServiceImpl file

---
+++ src/main/java/md4/bid_project/services/Impl/AuctionRecordServiceImpl.java
@@ -0,0 +1,45 @@
+package md4.bid_project.services.Impl;
+
+
+import md4.bid_project.models.AuctionRecord;
+import md4.bid_project.repositories.AuctionRecordRepository;
+import md4.bid_project.repositories.AuctionRepository;
+import md4.bid_project.services.AuctionRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuctionRecordServiceImpl implements AuctionRecordService {
+
+    @Autowired
+    AuctionRecordRepository auctionRecordRepository;
+
+
+    @Override
+    public List<AuctionRecord> getAllAuctionRecords() {
+        return auctionRecordRepository.findAll();
+    }
+
+    @Override
+    public AuctionRecord getAuctionRecordById(Long id) {
+        return auctionRecordRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void createAuctionRecord(AuctionRecord auctionRecord) {
+        auctionRecordRepository.save(auctionRecord);
+
+    }
+
+    @Override
+    public void editAuctionRecord(AuctionRecord auctionRecord) {
+        auctionRecordRepository.save(auctionRecord);
+    }
+
+    @Override
+    public void deleteAuctionRecord(Long id) {
+        auctionRecordRepository.deleteById(id);
+    }
+}
