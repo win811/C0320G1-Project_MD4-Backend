4f079f2abf47afd2fe43e130385ad547d64faf27
Tunt82 <tunguyen.engineer@gmail.com>
2020-08-12 16:18:46 +0700

    C-Tu update Catagory(models)

---
+++ src/main/java/md4/bid_project/models/Category.java
@@ -0,0 +1,34 @@
+package md4.bid_project.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id" )
+    private Long categoryId;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    public Category() {
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
