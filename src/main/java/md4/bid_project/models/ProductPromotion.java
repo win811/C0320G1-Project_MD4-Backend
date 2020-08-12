c95616bff3074e6a7844ee5f2e180a385c922868
NguyenDucThong <Nguyenducthong22>
2020-08-12 15:45:23 +0700

    C-Thong sua bang khuyen mai model

--- src/main/java/md4/bid_project/models/ProductPromotion.java
+++ src/main/java/md4/bid_project/models/ProductPromotion.java
@@ -1,10 +1,13 @@
 package md4.bid_project.models;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Table(name = "product_promotion")
+@Data
 public class ProductPromotion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,5 +26,85 @@ public class ProductPromotion {
     private Boolean flag = true;
 
     public ProductPromotion() {
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Long getDiscount_id() {
+        return discount_id;
+    }
+
+    public void setDiscount_id(Long discount_id) {
+        this.discount_id = discount_id;
+    }
+
+    public Integer getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(Integer product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getSaleDate() {
+        return saleDate;
+    }
+
+    public void setSaleDate(Date saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public Date getEndOfEvent() {
+        return endOfEvent;
+    }
+
+    public void setEndOfEvent(Date endOfEvent) {
+        this.endOfEvent = endOfEvent;
+    }
+
+    public Integer getSalePercent() {
+        return salePercent;
+    }
+
+    public void setSalePercent(Integer salePercent) {
+        this.salePercent = salePercent;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Boolean flag) {
+        this.flag = flag;
     }
 }
