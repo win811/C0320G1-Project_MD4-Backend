2a397d9180e2da36aabd38457f18b7fa01863c2b
hothaodn <hothaodn@gmail.com>
2020-08-12 11:47:32 +0700

    B-Thao update CartDetail

--- src/main/java/md4/bid_project/models/CartDetail.java
+++ src/main/java/md4/bid_project/models/CartDetail.java
@@ -17,69 +17,22 @@ public class CartDetail {
 //    @ManyToOne
 //    @JoinColumn(name = "cart_id", nullable=false)
 //    private Cart cart;
-//
-//    @ManyToOne
+
+//    @OneToOne
 //    @JoinColumn(name = "product_id", nullable=false)
 //    private Product product;
+//
+//    @OneToOne
+//    @JoinColumn(name = "auction_id")
+//    private Auction auction;
 
     @Column(name = "product_win_price")
-    private Long productWinPrice;
+    private double productWinPrice;
 
     @Column(name = "product_quantity")
-    private Long productQuantity;
+    private int productQuantity;
 
     @Column(name = "cart_detail_status")
-    private boolean status;
-
-    public CartDetail() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-//    public Cart getCart() {
-//        return cart;
-//    }
-//
-//    public void setCart(Cart cart) {
-//        this.cart = cart;
-//    }
-//
-//    public Product getProduct() {
-//        return product;
-//    }
-//
-//    public void setProduct(Product product) {
-//        this.product = product;
-//    }
-
-    public Long getProductWinPrice() {
-        return productWinPrice;
-    }
-
-    public void setProductWinPrice(Long productWinPrice) {
-        this.productWinPrice = productWinPrice;
-    }
-
-    public Long getProductQuantity() {
-        return productQuantity;
-    }
-
-    public void setProductQuantity(Long productQuantity) {
-        this.productQuantity = productQuantity;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
+    private String status;
 
 }
