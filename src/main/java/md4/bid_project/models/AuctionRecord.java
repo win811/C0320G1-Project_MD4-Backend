02fc74b183460eb57759e9780ae723ed6fc53718
nancynguyen14701 <ngannguyenqn2001@gmail.com>
2020-08-12 12:05:35 +0700

    C-Ngan update AuctionRecord entity

--- src/main/java/md4/bid_project/models/AuctionRecord.java
+++ src/main/java/md4/bid_project/models/AuctionRecord.java
@@ -2,41 +2,42 @@ package md4.bid_project.models;
 
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Set;
 
 @Entity
-@Table(name = "auctions_records")
+@Table(name = "auction_records")
+@Data
 public class AuctionRecord {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column (name = "record_id")
-    @Getter @Setter
     private Long id;
 
-    @Column (name = "record_auction_id")
-    @Getter @Setter
+    @JoinColumn(name = "record_auction_id")
+    @ManyToOne
     private Auction auction;
 
-    @Column (name = "record_bidder_id")
-    @Getter @Setter
+    @JoinColumn (name = "record_bidder_id")
+    @ManyToOne
     private User bidder;
 
     @Column (name = "record_bid_time")
-    @Getter @Setter
     private Date bidTime;
 
     @Column (name = "record_bid_price")
-    @Getter @Setter
     private Double bidPrice;
 
     @Column (name = "record_bid_is_winner")
-    @Getter @Setter
     private Boolean isWinner;
+
+
 
 }
