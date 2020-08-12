19d390d58e6c858f651badea00fc90c527ece36e
naki2204 <59548967+naki2204@users.noreply.github.com>
2020-08-12 11:15:37 +0700

    Delete AuctionRepository.java

--- src/main/java/md4/bid_project/repositories/AuctionRepository.java
+++
@@ -1,9 +0,0 @@
-package md4.bid_project.repositories;
-
-import md4.bid_project.models.Auction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuctionRepository extends JpaRepository<Auction,Long> {
-}
