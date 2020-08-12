d20731048e748f280e08c298e7a43e04462efad5
lamquoctung <bighero6ts@gmail.com>
2020-08-12 13:56:52 +0700

    update

--- src/main/java/md4/bid_project/services/Impl/RateServiceImpl.java
+++ src/main/java/md4/bid_project/services/Impl/RateServiceImpl.java
@@ -4,9 +4,11 @@ import md4.bid_project.models.Rate;
 import md4.bid_project.repositories.RateRepository;
 import md4.bid_project.services.RateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RateServiceImpl implements RateService {
 
     @Autowired
