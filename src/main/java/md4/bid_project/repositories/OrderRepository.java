34125fb7da96af92e52560e114ab81abe99ec362
quandang1996 <62531954+quandang1996@users.noreply.github.com>
2020-08-12 13:56:42 +0700

    Delete OrderRepository.java

--- src/main/java/md4/bid_project/repositories/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package md4.bid_project.repositories;
-
-import md4.bid_project.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-    Order findAllByUser_Id(Long id);
-}
