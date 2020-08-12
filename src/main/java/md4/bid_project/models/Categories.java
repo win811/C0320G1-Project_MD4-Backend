a2c487ad5b7930a315bb20d0c6b78949641d5a3e
Tunt82 <tunguyen.engineer@gmail.com>
2020-08-12 11:37:15 +0700

    Tu-Them entity Categories

---
+++ src/main/java/md4/bid_project/models/Categories.java
@@ -0,0 +1,25 @@
+package md4.bid_project.models;
+
+public class Categories {
+    private Long category_id;
+    private String category_name;
+
+    public Categories() {
+    }
+
+    public Long getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Long category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+}
