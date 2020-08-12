c0cbcfa38864adcfe159b982c19dd1493f9a7258
ADMIN <mauminhka19@gmail.com>
2020-08-12 13:46:30 +0700

    update UserRepository

--- src/main/java/md4/bid_project/repositories/UserRepository.java
+++ src/main/java/md4/bid_project/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package md4.bid_project.repositories;
 import md4.bid_project.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+public interface UserRepository extends  JpaRepository<User, Long>{
 }
