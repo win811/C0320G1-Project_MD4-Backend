26d0141875217025dbfdcf107729cc00182f3808
win811 <cuongwin811@gmail.com>
2020-08-13 21:51:05 +0700

    cuong- cap nhat master lan 2

--- src/main/java/md4/bid_project/controllers/ProductController.java
+++ src/main/java/md4/bid_project/controllers/ProductController.java
@@ -1,13 +1,24 @@
 package md4.bid_project.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import md4.bid_project.models.Product;
+import md4.bid_project.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products/owner/{ownerId}")
+    public ResponseEntity<List<Product>> getProductByOwnerId(@PathVariable(value = "ownerId") Long ownerId) {
+        return ResponseEntity.ok(productService.findProductByOwnerId(ownerId));
+    }
 
 }
--- src/main/java/md4/bid_project/services/impl/ProductServiceImpl.java
+++ src/main/java/md4/bid_project/services/impl/ProductServiceImpl.java
@@ -1,17 +1,21 @@
 package md4.bid_project.services.impl;
 
+import md4.bid_project.models.Product;
+import md4.bid_project.repositories.ProductRepository;
 import md4.bid_project.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    public void getSomething() {
-        return;
+    @Autowired
+    ProductRepository productRepository;
+    
+    @Override
+    public List<Product> findProductByOwnerId(Long ownerId) {
+        return productRepository.findByOwner_Id(ownerId);
     }
-
-    public void doSomething1(){
-        System.out.println("a");
-    }
-
 }
