3d9af79c32f8ba918e0912a0089f6ce7e0b7bd31
hanayuki <hanayuki1306@gmail.com>
2020-08-14 11:32:02 +0700

    C-Son update Product Service

--- src/main/java/md4/bid_project/services/ProductService.java
+++ src/main/java/md4/bid_project/services/ProductService.java
@@ -1,4 +1,11 @@
 package md4.bid_project.services;
 
+import md4.bid_project.models.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    List<Product> findAllProduct();
+    Product findProductById(Long id);
+    void saveProduct(Product product);
 }
