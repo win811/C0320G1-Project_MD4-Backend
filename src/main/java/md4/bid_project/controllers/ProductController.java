cf49398cde86d6629261a723eb1d91baa85d272d
hanayuki <hanayuki1306@gmail.com>
2020-08-14 11:29:51 +0700

    C-Son update ProductController

--- src/main/java/md4/bid_project/controllers/ProductController.java
+++ src/main/java/md4/bid_project/controllers/ProductController.java
@@ -1,13 +1,40 @@
 package md4.bid_project.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import md4.bid_project.models.Product;
+import md4.bid_project.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
+
 public class ProductController {
+    @Autowired
+    public ProductService productService;
 
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> listProduct(){
+        List<Product> pageProduct = productService.findAllProduct();
 
+        if (pageProduct.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Product>>(pageProduct,HttpStatus.OK);
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Void> createProduct(@RequestBody Product product){
+        productService.saveProduct(product);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+    @PutMapping("/products")
+    public ResponseEntity<Void> updateProduct(@RequestBody Product product){
+        productService.saveProduct(product);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
 }
