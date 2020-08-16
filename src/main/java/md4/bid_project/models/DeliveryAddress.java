51006500ee9a6727e631305d61bae5a7a5561dc0
NguHoaDuy <62531944+NguHoaDuy@users.noreply.github.com>
2020-08-16 14:06:33 +0700

    modified valid message

--- src/main/java/md4/bid_project/models/DeliveryAddress.java
+++ src/main/java/md4/bid_project/models/DeliveryAddress.java
@@ -20,24 +20,24 @@ public class DeliveryAddress {
     @Column(name = "delivery_nation")
     private String nation;
 
-    @NotBlank(message = "Tỉnh thành không được để trống")
+    @NotBlank(message = "Vui lòng nhập tỉnh thành")
     @Column(name = "delivery_city")
     private String city;
 
-    @NotBlank(message = "Quận huyện không được để trống")
+    @NotBlank(message = "Vui lòng nhập quận huyện")
     @Column(name = "delivery_district")
     private String district;
 
-    @NotBlank(message = "ward")
+    @NotBlank(message = "Vui lòng nhập phường xã")
     @Column(name = "delivery_ward")
     private String ward;
 
-    @NotBlank(message = "Phường xã không được để trống")
+    @NotBlank(message = "Vui lòng nhập địa chỉ giao hàng")
     @Column(name = "delivery_street")
     private String street;
 
     @NotBlank
-    @Column(name = "delivery_phone_number")
+    @Column(name = "Vui lòng nhập số điện thoại giao hàng")
     @Pattern(regexp = "^0[35789]\\d{8}$", message = "Số điện thoại không hợp lệ")
     private String phoneNumber;
 
