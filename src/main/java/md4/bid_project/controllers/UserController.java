fc1c926c26ee020d0458683e36f64cf658c268df
HuuHau0909 <huuhau0909@gmail.com>
2020-08-17 11:10:28 +0700

    push to review

--- src/main/java/md4/bid_project/controllers/UserController.java
+++ src/main/java/md4/bid_project/controllers/UserController.java
@@ -1,11 +1,60 @@
 package md4.bid_project.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import md4.bid_project.models.User;
+import md4.bid_project.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/userList")
+    public List<User> getUserList() {
+        return userService.getAllUser();
+    }
+
+    @PostMapping("/user-create")
+//    public void createUser(@RequestBody User user) {
+//        userService.saveUser(user);
+//    }
+    public ResponseEntity<User> createUser(@RequestBody User user, UriComponentsBuilder builder) {
+        userService.saveUser(user);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/user/{id}").buildAndExpand(user.getId()).toUri());
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @Valid @RequestBody User user) {
+        Optional<User> currentUser = userService.findById(id);
+        if (!currentUser.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        currentUser.get().setId(user.getId());
+        currentUser.get().setFullname(user.getFullname());
+        currentUser.get().setAddress(user.getAddress());
+        currentUser.get().setRate(user.getRate());
+        currentUser.get().setEmail(user.getEmail());
+        currentUser.get().setPhoneNumber(user.getPhoneNumber());
+        currentUser.get().setLastLogin(user.getLastLogin());
+        currentUser.get().setPoint(user.getPoint());
+        userService.saveUser(currentUser.get());
+        return new ResponseEntity<>(currentUser.get(),HttpStatus.OK);
+    }
 }
+
--- src/main/java/md4/bid_project/services/impl/UserServiceImpl.java
+++ src/main/java/md4/bid_project/services/impl/UserServiceImpl.java
@@ -1,9 +1,32 @@
 package md4.bid_project.services.impl;
 
+import md4.bid_project.models.User;
 import md4.bid_project.repositories.UserRepository;
 import md4.bid_project.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
 }
