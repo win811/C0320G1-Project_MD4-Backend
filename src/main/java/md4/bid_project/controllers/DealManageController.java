f4f8d97635c9f4b3a84f77dd168369c30e20c82d
hothaodn <hothaodn@gmail.com>
2020-08-18 05:15:45 +0700

    B-Thao updated

--- src/main/java/md4/bid_project/controllers/DealManageController.java
+++ src/main/java/md4/bid_project/controllers/DealManageController.java
@@ -3,6 +3,7 @@ package md4.bid_project.controllers;
 import md4.bid_project.models.dto.DealManageApi;
 import md4.bid_project.models.CartDetail;
 import md4.bid_project.models.dto.DealManageDTO;
+import md4.bid_project.repositories.CartDetailRepository;
 import md4.bid_project.services.CartDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -17,18 +18,20 @@ import java.util.List;
 import java.util.Map;
 
 //all created by Thao
-//@CrossOrigin(origins = "*", allowedHeaders = "*")
 @CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1")
 public class DealManageController {
 
     @Autowired
     private CartDetailService cartDetailService;
+    @Autowired
+    private CartDetailRepository cartDetailRepository;
 
     @GetMapping(value = "/deal-management")
     public ResponseEntity<DealManageApi> showAllNotDeletedDealList(@RequestParam("page") int currentPage,
-                                                   @RequestParam("limit") int pageSize) {
+                                                                   @RequestParam("limit") int pageSize) {
         Pageable pageable = PageRequest.of(currentPage-1, pageSize, Sort.by("id"));
         List<DealManageDTO> dealList = cartDetailService.findAllNotDeletedDeal(pageable);
         if (dealList.isEmpty()) {
@@ -54,7 +57,7 @@ public class DealManageController {
     @PutMapping(value = "/deal-management/delete")
     public Map<String, Boolean> deleteDeals(@RequestBody Long[] ids){
         Map<String, Boolean> response = new HashMap<>();
-        for(Long id : ids){
+        for(Long id : ids) {
             CartDetail deal = cartDetailService.findById(id);
             if(deal.getStatus().equals("Thành công") || deal.getStatus().equals("Thất bại") ){
                 deal.setIsDelete(true);
@@ -67,15 +70,31 @@ public class DealManageController {
         return response;
     }
 
+    @PutMapping(value = "/deal-management/delete/{id}")
+    public ResponseEntity<CartDetail> deleteOne(@PathVariable(value = "id") Long id) {
+        CartDetail deal = cartDetailService.findById(id);
+        if(deal.getStatus().equals("Thành công") || deal.getStatus().equals("Thất bại") ){
+            deal.setIsDelete(true);
+            final CartDetail deletedDeal = cartDetailService.save(deal);
+            return ResponseEntity.ok(deletedDeal);
+        } else {
+            return new ResponseEntity<CartDetail>(deal, HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
     @GetMapping(value = "/deal-management/search")
-    public ResponseEntity<DealManageApi> searchDeals(@RequestParam("namebuyer") String nameBuyer,
-                                                     @RequestParam("nameSeller") String nameSeller,
-                                                     @RequestParam("nameproduct") String nameProduct,
-                                                     @RequestParam("status") String status,
+    public ResponseEntity<DealManageApi> searchDeals(@RequestBody Map<String, Object> infoSearch,
                                                      @RequestParam("page") int currentPage,
                                                      @RequestParam("limit") int pageSize) {
         Pageable pageable = PageRequest.of(currentPage-1, pageSize, Sort.by("id"));
-        List<DealManageDTO> dealList = cartDetailService.searchDeals(nameBuyer, nameSeller, nameProduct, status);
+        String nameBuyer = (infoSearch.get("nameBuyer").toString());
+        String nameSeller = (infoSearch.get("nameSeller").toString());
+        String nameProduct = (infoSearch.get("nameProduct").toString());
+        String statusOfDeal = (infoSearch.get("statusOfDeal").toString());
+        Double totalPayment = Double.valueOf(infoSearch.get("totalPayment").toString());
+
+        List<CartDetail> dealList = cartDetailRepository.queryBy(nameBuyer, nameSeller, nameProduct, totalPayment, statusOfDeal);
         if (dealList.isEmpty()) {
             return new ResponseEntity<DealManageApi>(HttpStatus.NO_CONTENT);
         } else {
