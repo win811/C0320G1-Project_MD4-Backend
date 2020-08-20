ba478645befb331bb4657b5d9168c18ddd738afb
hung0701 <hung38k14@gmail.com>
2020-08-20 11:51:56 +0700

    commit truoc khi pull thien

--- src/main/java/md4/bid_project/BidProjectApplication.java
+++ src/main/java/md4/bid_project/BidProjectApplication.java
@@ -3,12 +3,13 @@ package md4.bid_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class BidProjectApplication  {
 
     public static void main(String[] args) {
