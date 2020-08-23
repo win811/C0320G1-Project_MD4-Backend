c2cecf0dc7a3732535fc47e43b5fb37bb4d2443f
quandang1996 <quandanght@gmail.com>
2020-08-23 22:50:01 +0700

    save

--- src/main/java/md4/bid_project/security/WebSecurityConfig.java
+++ src/main/java/md4/bid_project/security/WebSecurityConfig.java
@@ -51,13 +51,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests().antMatchers("/**", "api/v1/login", "api/v1/home", "api/v1/register").permitAll()
+                .authorizeRequests().antMatchers( "/api/v1/login", "api/v1/home", "api/v1/register").permitAll()
                 .and()
-                .authorizeRequests().antMatchers("api/v1/payment/**").access("hasRole('ROLE_USER')")
+                .authorizeRequests().antMatchers("/api/v1/payment/**","/api/v1/cart/**").access("hasRole('ROLE_USER')")
                 .and()
-                .authorizeRequests().antMatchers("api/v1/admin/**").access("hasRole('ROLE_ADMIN')")
+                .authorizeRequests().antMatchers("/api/v1/admin/**").access("hasRole('ROLE_ADMIN')")
                 .and()
-                .authorizeRequests().antMatchers("api/v1/products/**").access("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
+                .authorizeRequests().antMatchers("/api/v1/products/**").access("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
                 .anyRequest().authenticated()
                 .and().cors();
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
